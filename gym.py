b88c3f57a0726cf50e3c3102dedea03e14390750
Maxos <pelsh2002@gmail.com>
2026-06-20 14:16:53 +0300

    completed CRUD functionality

--- gym.py
+++ gym.py
@@ -1,16 +1,41 @@
 import database
-from models import ExerciseCreateDTO, ExersiceEntity
+import schemas
+from models import ExerciseEntity
 
-def create_exercise(dto: ExerciseCreateDTO):
-    entity = ExersiceEntity(
+def create_exercise(dto: schemas.ExerciseCreate):
+    catalog = database.exercises.find()
+    entity = ExerciseEntity(
         name=dto.name,
         date=dto.date,
         weight=dto.weight,
         reps=dto.reps
     )
-    database.exercises_collection.insert_one({
-        "name": dto.name,
-        "date": dto.date,
-        "weight": dto.weight,
-        "reps": dto.reps
+    database.exercises.insert_one({
+        "name": entity.name,
+        "date": entity.date,
+        "weight": entity.weight,
+        "reps": entity.reps
     })
+
+def show_exercises():
+    diary = database.exercises.find()
+    result = []
+    for exercise in diary:
+        entity = ExerciseEntity(
+            name=exercise["name"],
+            date=exercise["date"],
+            weight=exercise["weight"],
+            reps=exercise["reps"]
+        )
+        result.append(entity)
+    return result
+
+def delete_exercise(dto: schemas.ExerciseGet):
+    exercise = database.exercises.delete_one({"name": dto.name})
+    return exercise
+
+def update_exercise(dto: schemas.ExerciseUpdateWeight):
+    database.exercises.update_one(
+        {"name": dto.name},
+        {"$set": {"weight": dto.weight, "reps": dto.reps}},
+    )
--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI
+from pygments.lexers import data
 
 import gym
-from requests import ExerciseCreate
-import models
+import schemas
+from schemas import ExerciseCreate, ExerciseGet, ExerciseUpdateWeight, ExerciseUpdateWeightRequest
 
 app = FastAPI(title="Gym Tracker")
 
@@ -11,11 +12,23 @@ app = FastAPI(title="Gym Tracker")
 def root():
     return {"message": "Gym Tracker API"}
 
-@app.post("/exercise")
-def create_exercise(data: ExerciseCreate):
-    dto = models.ExerciseCreateDTO(
-        data.name, data.date, data.weight, data.reps
-    )
+@app.post("/exercises")
+def add_exercise(data: ExerciseCreate):
+    gym.create_exercise(data)
+    return {"exercise added": data}
 
-    exercise = gym.create_exercise(dto)
-    return {"exercise": exercise}
+@app.get("/exercises")
+def show_exercises():
+    exercises = gym.show_exercises()
+    return {"exercises": exercises}
+
+@app.delete("/exercises/{name}")
+def delete_exercise(data: ExerciseGet):
+    gym.delete_exercise(data)
+    return {"exercise deleted": data}
+
+@app.put("/exercises/{name}")
+def update_exercise(name, data: ExerciseUpdateWeightRequest):
+    dto = schemas.ExerciseUpdateWeight(name=name, weight=data.weight, reps=data.reps)
+    updated = gym.update_exercise(dto)
+    return {"exercise updated": updated}
